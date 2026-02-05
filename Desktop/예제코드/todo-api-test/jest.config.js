ae030f21c0abf2fec55c63ab365d71e0f62099f0
Jueun Lee <wndms328@naver.com>
2026-02-05 13:09:26 +0900

    Remove nested duplicate project

--- Desktop/예제코드/todo-api-test/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  verbose: true,
-  testMatch: ['**/*.test.ts'],
-  transform: {
-    '^.+\\.ts$': 'ts-jest',
-  },
-  moduleFileExtensions: ['ts', 'js'],
-};
